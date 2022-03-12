3ef7b0b3995959ad63534486edb190a45d082cc2
eriveltoncorsino <eriva.rs@gmail.com>
2022-03-12 09:19:33 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=43): # * filhos quer dizer uma quantidade variável de filhos
+    def __init__(self, *filhos, nome=None, idade=55): # * filhos quer dizer uma quantidade variável de filhos
         self.idade = idade
         self.nome = nome
         self.filhos = list (filhos)
@@ -10,7 +10,7 @@ class Pessoa:
 
 if __name__ == '__main__':
     Lorenzo = Pessoa(nome ='Lorenzo') # o objeto complexo "lorenzo" é do tipo pessoa,
-    Silvana = Pessoa(Lorenzo, nome='Silvana') # e está passando ele para o atributo "Sivana"
+    Silvana = Pessoa(Lorenzo, nome='Silvana') # e está passando ele para o atributo "Silvana"
     print(Pessoa.cumprimentar(Silvana))
     print(id(Silvana))
     print(Silvana.cumprimentar())
@@ -18,6 +18,10 @@ if __name__ == '__main__':
     print(Silvana.idade)
     for filho in Silvana.filhos: # para cada filho nos filhos deste objeto "Luciano" vou imprimir o filho.nome
         print(filho.nome)
-
+    Silvana.sobrenome = 'Moreira'
+    del Silvana.filhos
+    print(Silvana.sobrenome)
+    print(Silvana.__dict__)
+    print(Lorenzo.__dict__)
 
 
