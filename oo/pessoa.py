2cf72a80c83ca0a9f423aab99e6f628304a4e5df
eriveltoncorsino <eriva.rs@gmail.com>
2022-03-12 08:53:29 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,23 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=43):
+    def __init__(self, *filhos, nome=None, idade=43): # * filhos quer dizer uma quantidade variável de filhos
         self.idade = idade
         self.nome = nome
+        self.filhos = list (filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Silvana')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'erivelton'
-    print(p.nome)
+    Lorenzo = Pessoa(nome ='Lorenzo') # o objeto complexo "lorenzo" é do tipo pessoa,
+    Silvana = Pessoa(Lorenzo, nome='Silvana') # e está passando ele para o atributo "Sivana"
+    print(Pessoa.cumprimentar(Silvana))
+    print(id(Silvana))
+    print(Silvana.cumprimentar())
+    print(Silvana.nome)
+    print(Silvana.idade)
+    for filho in Silvana.filhos: # para cada filho nos filhos deste objeto "Luciano" vou imprimir o filho.nome
+        print(filho.nome)
+
 
 
